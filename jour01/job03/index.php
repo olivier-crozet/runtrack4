4e4f869cce1d5117cc1287d8e2cd1053102c52f2
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-04 15:25:11 +0200

    job2 a moitier

---
+++ jour01/job03/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+			
+		<link rel="stylesheet" type="text/css"    href="style.css"/>
+		
+
+		<title>job01</title>
+	</head>
+	<body>
+
+
+		
+	
+	   <section >
+	   </section>
+
+	       <section >
+		
+	       </section>
+
+	           <section class="section" >
+		
+	           </section>
+
+	           <section>
+	           	
+
+	           </section>
+
+	        
+
+
+	
+	</body>
+	</html>
+</DOCTYPE>
