adc8018f24a557bd65cfbcbcf6f6594bc7265396
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-04 12:48:42 +0200

    job02 fini

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+			
+		<link rel="stylesheet" type="text/css"    href="style1.css"/>
+		
+
+		<title>job01</title>
+	</head>
+	<body>
+		<h1>
+			TEST
+		</h1>
+	
+	</body>
+	</html>
+</DOCTYPE>
