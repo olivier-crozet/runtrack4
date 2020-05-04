8348ab0afa434871c796c83ce025b2984b693068
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-04 11:54:48 +0200

    test

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+
+	<!DOCTYPE html>
+	<html>
+	<head>
+			
+		<link rel="stylesheet" type="text/css" media="screen and (min-width: 1920px) "   href="style1.css"/>
+		<link rel="stylesheet" type="text/css"  media="screen and (max-width: 1920px) and (min-width: 1680px) " href="style2.css"/>
+		<title>job01</title>
+	</head>
+	<body>
+		<h1>
+			TEST
+		</h1>
+	
+	</body>
+	</html>
+</DOCTYPE>
