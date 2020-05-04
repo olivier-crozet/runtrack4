230a1ac8a28123bfd70d6470a59e5119e85f39fe
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-04 12:10:03 +0200

    job 1 fini

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,7 +4,10 @@
 	<head>
 			
 		<link rel="stylesheet" type="text/css" media="screen and (min-width: 1920px) "   href="style1.css"/>
-		<link rel="stylesheet" type="text/css"  media="screen and (max-width: 1920px) and (min-width: 1680px) " href="style2.css"/>
+		<link rel="stylesheet" type="text/css"  media="screen and (max-width: 1920px) and (min-width: 1681px) " href="style2.css"/>
+		<link rel="stylesheet" type="text/css"  media="screen and (max-width: 1680px) and (min-width: 1280px) " href="style3.css"/>
+		<link rel="stylesheet" type="text/css" media="screen and (max-width: 1280px) "   href="style4.css"/>
+
 		<title>job01</title>
 	</head>
 	<body>
