89a90e3a609d0edc7f816085b50841bf1db2d1c8
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-04 23:51:37 +0200

    a finir

---
+++ jour01/job04/hamburger.js
@@ -0,0 +1,18 @@
+
+    $(document).ready(function(){ getElementById("open").hide() });
+     $(".button").click(function(){
+        $(".burger").show();
+          
+        });
+    
+    $("#cacher").click(function(){
+        $("h1").hide(5000, "linear", function(){
+   
+    });
+
+});
+
+/*$(".hamburger").click(fuction() {
+
+	$(this).toggleClass("is-active")
+}); */
---
+++ jour01/job04/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta name="viewport" content="width=device-width,initial-scale=1"><!--remet par defaut les taille pour enlever la protection d'affichage minimum -->
+		<link rel="stylesheet" type="text/css"    href="style.css"/>
+		<script type="text/javascript" src="hamburger.js"></script>
+
+		<title>job01</title>
+	</head>
+	<body>
+
+
+		
+	
+	<header>
+		<div id="clicmenu" >
+		<ul>
+			<li>
+				accueil
+			</li>
+			<li>
+				presentation
+			</li>
+			<li>
+				contact
+			</li>
+		</ul>
+	    </div>
+
+	    <div id ="burger" class="hamburger">
+	    	<span class ="ligne"></span>
+	    	<span class ="ligne"></span>
+	    	<span class ="ligne"></span>
+
+	    </div>
+	    <div id="open" class="openmenu">
+	    	gddfgs
+	    </div>
+
+
+
+
+
+	</header>
+	        
+
+
+	
+	</body>
+	</html>
+</DOCTYPE>
