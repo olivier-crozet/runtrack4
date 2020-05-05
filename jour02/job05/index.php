663858c509d776224184804387e3285ec142ed28
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-05 22:04:01 +0200

    job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,124 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+       <!--pour java script-->
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <!--Import materialize.css-->
+     <!--<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <header >
+         <nav  >
+
+    <div  class="  cyan darken-3 nav-wrapper" >
+      <a  href="#" class="brand-logo right"> <form>
+        <div class="input-field">
+          <input id="search" type="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form></a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+      	<li style="width: 10%"><a style="width: 100%"  href="" ><i style="width: 100%"  class="material-icons"><img style="width: 100%" src="images/aileron.jpg"></i></a></li>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Connection</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+      </header>
+      <body>
+    <br/>
+    <br/>
+      <article class="row"  >
+  	<div  >
+    <div class="col s12 m6">
+      <div class="card ">
+        <div class="card-image">
+          <img src="images/windsurf.jpg">
+          <span class="card-title">En windsurf</span>
+          <a href="https://www.pacific-palissades.com/" class="btn-floating halfway-fab waves-effect waves-light red"><i class="valign-wrapper" class="material-icons">+</i></a>
+        </div>
+        <div class="card-content">
+          <p>very tecknic wave spot by north west.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+  		<div class="row">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/kite.jpg">
+          <span class="card-title">En Kitesurf</span>
+          <a href="https://www.google.com/maps/place/Pointe-Rouge,+Marseille/@43.2508899,5.3663345,15.19z/data=!4m5!3m4!1s0x12c9c7784ca8a67f:0x13426002c6e8806a!8m2!3d43.2406037!4d5.3744934" class="btn-floating halfway-fab waves-effect waves-light red"><i class="valign-wrapper" class="material-icons">+</i></a>
+        </div>
+        <div class="card-content">
+          <p>very tecknic wave spot by north west.</p>
+        </div>
+      </div>
+    </div>
+  </div>  
+  </article>
+
+
+ <article>
+ <div class="row">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/windfoil.jpg">
+          <span class="card-title">En Kitesurf</span>
+          <a href="https://www.google.com/maps/place/Pointe-Rouge,+Marseille/@43.2508899,5.3663345,15.19z/data=!4m5!3m4!1s0x12c9c7784ca8a67f:0x13426002c6e8806a!8m2!3d43.2406037!4d5.3744934" class="btn-floating halfway-fab waves-effect waves-light red"><i class="valign-wrapper" class="material-icons">+</i></a>
+        </div>
+        <div class="card-content">
+          <p>very tecknic wave spot by north west.</p>
+        </div>
+      </div>
+    </div>
+  </div>  	
+ </article>
+
+      </div>
+       <footer class="cyan darken-3 page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://olivier-crozet.students-laplateforme.io/">Développeur</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://github.com/olivier-crozet">GIT</a></li>
+               <br/>
+               <br/>
+               <br/>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+    </body>
+  </html>
