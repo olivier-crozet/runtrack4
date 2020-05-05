fc6d011037eaa1fbc2fbfcbe2ea94112e494f87a
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-05 11:44:18 +0200

    passage en cdn

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -3,19 +3,24 @@
     <head>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+       <!--pour java script-->
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+     <!--<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
-      <header>
-         <nav>
+      <header >
+         <nav  >
 
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo right"> <form>
+    <div  class="  cyan darken-3 nav-wrapper" >
+      <a  href="#" class="brand-logo right"> <form>
         <div class="input-field">
           <input id="search" type="search" required>
           <label class="label-icon" for="search"><i class="material-icons">search</i></label>
