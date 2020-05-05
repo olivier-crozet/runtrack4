783b015629df92a4b2a9abc06553d5164b9b11de
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-05 19:52:19 +0200

    Job 3

---
+++ jour02/bonus/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo</title>
+	    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+	<nav  class="blue-grey darken-1">
+    <div class="nav-wrapper">
+      <a  href="#" class="brand-logo">ou rider ??</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  <article class="row"  >
+  	<div  >
+    <div class="col s12 m6">
+      <div class="card ">
+        <div class="card-image">
+          <img src="images/windsurf.jpg">
+          <span class="card-title">En windsurf</span>
+          <a href="https://www.pacific-palissades.com/" class="btn-floating halfway-fab waves-effect waves-light red"><i class="valign-wrapper" class="material-icons">+</i></a>
+        </div>
+        <div class="card-content">
+          <p>very tecknic wave spot by north west.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  	<div class="row">
+    <div class="col s12 m6">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/main-kite.jpg">
+          <span class="card-title">En Kitesurf</span>
+          <a href="https://www.google.com/maps/place/Pointe-Rouge,+Marseille/@43.2508899,5.3663345,15.19z/data=!4m5!3m4!1s0x12c9c7784ca8a67f:0x13426002c6e8806a!8m2!3d43.2406037!4d5.3744934" class="btn-floating halfway-fab waves-effect waves-light red"><i class="valign-wrapper" class="material-icons">+</i></a>
+        </div>
+        <div class="card-content">
+          <p>very tecknic wave spot by north west.</p>
+        </div>
+      </div>
+    </div>
+  </div>  
+  </article>
+</body>
+</html>
