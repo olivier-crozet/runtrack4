412296f1fa5b155972ff3cfcd991ff67b68ce7c4
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-07 01:16:35 +0200

    a finir

---
+++ jour03/job01/index.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!--cdn bootstrap js ?-->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+<style>
+   body{
+     background-color: #c3c1c1;
+   }
+   .oc-marge
+   {
+   	margin: 0;
+   }
+   .oc-gris
+   {
+   	background-color:#dcdbdb;
+   }
+   </style>
+	<title>job01</title>
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">LPTFr</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Units</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jobs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Skills</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
+<main  >
+
+	<h1 class="display-3  text-secondary text-center  ">LaPlateforme_</h1>
+
+	<article class="row justify-content-around col-sm-12  " > 
+		     <div class="p-0 card   col-md-12-m-auto col-lg-2" >
+		     	<img src="images/papillon.jpg" class="card-img-top" alt="...">
+		     	<div class="card-body">
+		     		<h5 class="card-title">Card title</h5>
+		     		<p class="card-text">un papillon c'est un peu comme une chenille, mais avec des ailes.<br/>
+		     		Ne pas ingérer.</p>
+		     		<a href="#" class="btn btn-primary">Go somewhere</a>
+		     	</div>
+		     </div>
+
+
+		     <div class="col-lg-6 col-md-6 mt-md-3 p-0 card ">
+		     	<div class="card-body oc-gris col-sm-12  ">
+		     		<h5 class=" display-4 card-title">Bonjour, monde!</h5>
+		     		<p class="card-text">
+		     			il existe plusieur version du theme:<br/>
+		     			le monde est la matière,l'espace et les phénomenessui nous sont accessibles par les sens. l'experience ou la raison.<br/>
+		     			Le sens le plus coutant designe notre planète la Terre avec ses habitants et son environement plus ou moins naturel. </p><hr class="my-4">
+		     			<p>Le sens étendu dédigne l'univers dans emsemble</p>
+		     		<button type="button" class="btn btn-danger">Rebooter le monde</button><br>
+		     		<nav aria-label=" Page navigation example ">
+  <ul class="d-flex justify-content-end pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+		     	</div>
+		     </div>
+
+		    
+<div class="row col-lg-2 col-sm-12 col-md-4 mt-md-3 px-2" >
+    <div class="list-group" id="list-tab" role="tablist">
+      <a style="width: 18rem;" class=" list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Limbes</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Luxure</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Gourmandise</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Avarice</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Colère</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">herisie</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Ruse Tromperie</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Trahison</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Internet explorer</a>
+
+    </div>
+  </div>
+  </article>
+  <br/>
+ 
+  	
+  >
+  <article class="col-5 m-auto">
+ <P class="font-weight-bold text-right">Instalation de AI 9000</P><br/>
+  	<div class=" m-auto  progress">
+  		
+  <div class=" col-11 progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+  </article>
+  <br/>
+
+
+  <article>
+  	 <label class="sr-only " for="inlineFormInputGroupUsername2">Username</label>
+  	<p class="justify-content-around">  Recever votre copie gratuite d'internet 2.</p>
+  <div class="p-0 input-group col-lg-6 mb-2 mr-sm-2">
+    <div class="input-group-prepend">
+      <div class="input-group-text">@</div>
+     
+    </div>
+    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+  </div>
+  <br/>
+
+  <div class="custom-file col-lg-6 mb-2 mr-sm-2">
+  <input type="file" class="custom-file-input" id="customFileLangHTML">
+  <label class="custom-file-label" for="customFileLangHTML" data-browse="Bestand kiezen">Voeg je document toe</label>
+</div>
+
+<label class="sr-only " for="inlineFormInputGroupUsername2">Username</label>
+  	<p class="justify-content-around">  Recever votre copie gratuite d'internet 2.</p>
+  <div class="p-0 input-group col-lg-6 mb-2 mr-sm-2">
+    <div class="input-group-prepend">
+      <div class="input-group-text">@</div>
+        </div>
+    <input class="" type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+  
+	</div>
+	
+	
+
+  </article>
+</main>
+</body>
+
+</html>
