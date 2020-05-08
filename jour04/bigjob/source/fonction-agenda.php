d2897d776c15533d9c50fcdf124aaacf53e6194e
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-09 00:56:57 +0200

    date marche bien

--- jour04/bigjob/source/fonction-agenda.php
+++ jour04/bigjob/source/fonction-agenda.php
@@ -8,18 +8,20 @@ class Date
 	function getAll($year)
 	{
 		$r =  array();
+		//recuperation du premier jours de l'année
 		$date =strtotime($year.'-01-01');   
-
+		//fini en fin d'anner
 		    while(date('y',$date) <= $year) 
 		    {
 		    
 
 		$y = date('y',$date);
-		$m = date('m',$date);
-		$d = date('d',$date);
+		$m = date('n',$date);//le n enleve le zero devant le 01 du mois
+		$d = date('j',$date);// le j enleve le zero devant le 01 du jour
 		//change deux jour pour les avoir de 0 a 7
 		$w = str_replace('0','7',date('w',$date));
 		$r[$y][$m][$d] = $w;
+		//+ 24h *3600 segonde
 		$date = $date + 24 * 3600;
           	}
 		return $r;
