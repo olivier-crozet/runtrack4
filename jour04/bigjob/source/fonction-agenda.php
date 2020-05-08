cb011df883b2cd271fdf63aeb2189108456e6e76
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-09 01:16:10 +0200

    boucle agenda version objet o top

--- jour04/bigjob/source/fonction-agenda.php
+++ jour04/bigjob/source/fonction-agenda.php
@@ -8,22 +8,43 @@ class Date
 	function getAll($year)
 	{
 		$r =  array();
+
+		//////////////////////////////////////////////VERSION NON OBJET QUI MARCHE //////////////////////////////
 		//recuperation du premier jours de l'année
-		$date =strtotime($year.'-01-01');   
+		//$date =strtotime($year.'-01-01');   
 		//fini en fin d'anner
-		    while(date('y',$date) <= $year) 
-		    {
+		  //  while(date('y',$date) <= $year) 
+		   // {
 		    
 
-		$y = date('y',$date);
-		$m = date('n',$date);//le n enleve le zero devant le 01 du mois
-		$d = date('j',$date);// le j enleve le zero devant le 01 du jour
+		//$y = date('y',$date);
+		//$m = date('n',$date);//le n enleve le zero devant le 01 du mois
+		//$d = date('j',$date);// le j enleve le zero devant le 01 du jour
 		//change deux jour pour les avoir de 0 a 7
-		$w = str_replace('0','7',date('w',$date));
+		//$w = str_replace('0','7',date('w',$date));
+		//$r[$y][$m][$d] = $w;
+		//+ 24h *3600 segonde pour les anneer avec normal
+        //$date + 24 * 3600;
+		//ca c'est pour les bisextil
+		//$date = strtotime(date('y-m-d',$date).'+1 DAY'); 
+		   // }
+
+///////////////////////////////////////:version objet attention a la mise a jour ovh////////////
+		    $date = new DateTime($year.'-01-01');
+		    while($date->format('y') <= $year) 
+		    {
+		    
+		    $y = $date->format('y');
+		    $m = $date->format('n');//le n enleve le zero devant le 01 du mois
+		    $d = $date->format('j');// le j enleve le zero devant le 01 du jour
+		//change deux jour pour les avoir de 0 a 7
+		$w = str_replace('0','7',$date->format('w'));
 		$r[$y][$m][$d] = $w;
-		//+ 24h *3600 segonde
-		$date = $date + 24 * 3600;
-          	}
+		//creer un nouvel objet avec P1D pour + un jour
+		$date->add(new DateInterval('P1D'));
+	
+		   }
+          	
 		return $r;
 	}
 }
