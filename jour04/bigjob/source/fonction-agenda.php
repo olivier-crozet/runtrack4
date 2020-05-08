99f5a022bcfc3710d1b77824c138ca912c2bdd54
olivier-crozet <olivier.crozet@laplateforme.io>
2020-05-09 00:37:32 +0200

    boucle agenda ok

--- jour04/bigjob/source/fonction-agenda.php
+++ jour04/bigjob/source/fonction-agenda.php
@@ -10,13 +10,18 @@ class Date
 		$r =  array();
 		$date =strtotime($year.'-01-01');   
 
+		    while(date('y',$date) <= $year) 
+		    {
+		    
+
 		$y = date('y',$date);
 		$m = date('m',$date);
 		$d = date('d',$date);
 		//change deux jour pour les avoir de 0 a 7
 		$w = str_replace('0','7',date('w',$date));
 		$r[$y][$m][$d] = $w;
-
+		$date = $date + 24 * 3600;
+          	}
 		return $r;
 	}
 }
